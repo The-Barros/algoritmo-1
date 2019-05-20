77f57d02f9bace6ab42dbf35074d3d7a2c6d6686
Icaro1030 <37670033+Icaro1030@users.noreply.github.com>
2019-05-20 15:41:25 -0400

    Update and rename CodigoServidor.java to ServeCode.java

--- CodigoConexao/ServeCode.java
+++ CodigoConexao/ServeCode.java
@@ -1,22 +1,14 @@
 import java.io.*;
 import java.net.*;
 
-import lejos.hardware.lcd.LCD;
-
 public class NewServer {
 	
 	public static final int port = 1234;
 	
 	public static void main(String [] args) throws IOException, InterruptedException {
-
-		EV3TouchSensor sensor = new EV3TouchSensor(SensorPort.S4);
-		TouchAdapter touch = new TouchAdapter(sensor);
-		
+	
 		ServerSocket server = new ServerSocket(port);
-		LCD.clear();
-		LCD.drawString("Aguardando Cliente!", 0, 4);
 		Socket client = server.accept(); // Espera chamada do cliente
-		LCD.clear();
-		LCD.drawString("Conectado!",0,4);	
+		Syste.out.println("Conectado!");
 	}
 }
